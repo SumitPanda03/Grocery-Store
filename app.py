7ba0249bfb06602e5d607c4a04ca01ce9f0de3bc
Sumit_Panda <95120820+SumitPanda03@users.noreply.github.com>
2023-06-29 14:55:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, session
 from pymongo import MongoClient
+from flask_login import login_manager, UserMixin, login_user, logout_user, login_required, LoginManager
+from werkzeug.security import generate_password_hash,check_password_hash
+import os
 
 
 app = Flask(__name__, template_folder='templates')
+app.secret_key = 'ad15a082fe69aad481086b06c43001e4a347e35a28d17e3f9f0b2c35e3fafe07'
+mongo_uri = os.environ.get('MONGO_URI')
 client = MongoClient('mongodb://localhost:27017')
 db = client['Store']
-items_db= db['items']
+items_db = db['items']
 customers_db = db['customers']
+auth_db = db['auth']
+
+# login_manager = LoginManager()
+# # login_manager.login_view = 'login'
+# login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user():
+    return auth_db.get(auth_db['_id'])
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/signup', methods=["POST"])
+def signup_post():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    auth_db.insert_one({'name': name, 'email': email, 'password': generate_password_hash(password,method='sha256')})
+
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=["POST"])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user_details = auth_db.find_one({'email': email})
+    if check_password_hash(user_details['password'], password):
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/')
@@ -65,14 +110,14 @@ def buy():
 def cart():
     bought_items = customers_db.find({})
     item_billing = customers_db.find({})
-
+    user_details = auth_db.find({})
     net_price = 0
     net_quantity = 0
     for i in item_billing:
         net_quantity = net_quantity + int(i['QUANTITY'])
         net_price = net_price + (int(i['PRICE']) * int(i['QUANTITY']))
 
-    return render_template('cart.html', bought_items=bought_items, net_price=net_price, net_quantity=net_quantity)
+    return render_template('cart.html', bought_items=bought_items, net_price=net_price, net_quantity=net_quantity,user_details=user_details)
 
 
 @app.route('/reset')
